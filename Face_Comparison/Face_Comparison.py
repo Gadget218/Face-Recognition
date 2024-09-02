baf00b9f7934d2407e7eb3839959fc4a73936344
Affan218 <103178361+Gadget218@users.noreply.github.com>
2024-09-02 13:35:27 +0530

    Update Face_Comparison.py

--- Face_Comparison/Face_Comparison.py
+++ Face_Comparison/Face_Comparison.py
@@ -11,9 +11,16 @@ img2=cv2.imread(r"images\Messi.webp")
 rgb_img2=cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
 img_encoding2 = face_recognition.face_encodings(rgb_img2)[0]
 
+img3=cv2.imread(r"images\Elon Musk.jpg")
+rgb_img3=cv2.cvtColor(img3, cv2.COLOR_BGR2RGB)
+img_encoding3 = face_recognition.face_encodings(rgb_img3)[0]
+
 #Let's compare the two faces to see if they are the same
 result=face_recognition.compare_faces([img_encoding],img_encoding2)
-print("Result: ",result)
+print("Result for Image 1 and 2: ",result)
+
+result1=face_recognition.compare_faces([img_encoding],img_encoding3)
+print("Result for Image 1 and 3: ",result1)
 
 cv2.imshow("Img", img) #to see the image
 cv2.imshow("Img 2", img2)
